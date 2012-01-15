bcf4b8ac96050752cb195e6e406ba4f35f3b9617
palle torsson <palle.torsson@gmail.com>
2012-01-15 17:31:08 +0100

    got ajax working, game.php only allow some moves, for now with xml, JSON later, javascript needs cleaning

--- app/cache/dev/twig/28/fc/de42d158ae091c4c470a08d6299d.php
+++ app/cache/dev/twig/28/fc/de42d158ae091c4c470a08d6299d.php
@@ -26,7 +26,11 @@ class __TwigTemplate_28fcde42d158ae091c4c470a08d6299d extends Twig_Template
     public function block_sidebar($context, array $blocks = array())
     {
         // line 5
-        echo "   Here vi will put info start button and alike. 
+        echo "<div id=\"start\">Start-button. </div></br></br>
+\t
+
+<div id=\"moves\">Game Moves: </br></br> </div>
+
 ";
     }
 
---
+++ src/Bundle/ChessBundle/Entity/Game.php
@@ -0,0 +1,31 @@
+<?php
+// we'll generate XML output
+header('Content-Type: text/xml');
+// generate XML header
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+// create the <response> element
+echo '<response>';
+// retrieve the user name
+$string = $_GET['text'];
+// $string = strtolower($string);
+$pattern = "/^[A-H]{1}+[0-9]{1}[-][A-H]{1}+[0-9]{1}/";
+// explode hand check that the input was of avalide type (a4-b6)
+// check taht letter and number in right order.
+if (preg_match($pattern, $string)) {
+   // check if moved farward and return string to board
+   if ($string[1]+1 == $string[4]) { 
+       $string_array = explode('-', $string);
+       $from = $string_array[0]; 
+       $to = $string_array[1];
+       echo $from.":".$to; 
+   } else {
+		return false; // "invalide move";
+   }
+   
+} else {
+    return false; // "invalide syntax";
+}
+
+// close the <response> element
+echo '</response>';
+?>
