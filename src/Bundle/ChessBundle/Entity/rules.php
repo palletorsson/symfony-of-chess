ade4789c38e95483963c1713921cf199894bd121
geard8 <nicke.tor.86@gmail.com>
2012-01-19 10:51:33 +0100

    rättat pjäserna på Game.php och börjat på rules.php
    Signed-off-by: geard8 <nicke.tor.86@gmail.com> + johan

---
+++ src/Bundle/ChessBundle/Entity/rules.php
@@ -0,0 +1,58 @@
+<?php
+    
+    
+    function checkMove($piece, $from, $to){
+    	//black pawn
+		if($piece == 9823){
+			
+		}
+		//white pawn
+		if($piece == 9817){
+			
+		}
+		//rook
+		if($piece == 9820 || $piece == 9814){
+			
+		}
+		//bishop
+		if($piece == 9821 || $piece == 9815){
+			
+		}
+		//knight
+		if($piece == 9822 || $piece == 9816){
+			
+		}
+		//queen
+		if($piece == 9819 || $piece == 9813){
+			
+		}
+		//king
+		if($piece == 9818 || $piece == 9812){
+			
+		}
+		
+		
+		
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+?>
