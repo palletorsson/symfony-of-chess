d4f2943d069169396fd6976f51b3bdec72e491b1
geard8 <nicke.tor.86@gmail.com>
2012-01-19 17:48:18 +0100

    Massa bug fixs i rules.php och kommer förmodligen behöve fler men nu är klockan snart 6 och kristan slänger då ut mig så inget mer idag
    Signed-off-by: geard8 <nicke.tor.86@gmail.com>

--- src/Bundle/ChessBundle/Entity/rules.php
+++ src/Bundle/ChessBundle/Entity/rules.php
@@ -2,6 +2,93 @@
     
     function checkMove($piece, $from, $to){
     	
+		$board = array(
+			'a8' => 9820,
+			'b8' => 9822,
+			'c8' => 9821,
+			'd8' => 9819,
+			'e8' => 9818,
+			'f8' => 9821,
+			'g8' => 9822,
+			'h8' => 9820,
+			
+			'a7' => 9823,
+			'b7' => 9823,
+			'c7' => 9823,
+			'd7' => 9823,
+			'e7' => 9823,
+			'f7' => 9823,
+			'g7' => 9823,
+			'h7' => 9823,
+			
+			'a6' => 0,
+			'b6' => 0,
+			'c6' => 0,
+			'd6' => 0,
+			'e6' => 0,
+			'f6' => 0,
+			'g6' => 0,
+			'h6' => 0,
+			
+			'a5' => 0,
+			'b5' => 0,
+			'c5' => 0,
+			'd5' => 0,
+			'e5' => 0,
+			'f5' => 0,
+			'g5' => 0,
+			'h5' => 0,
+			
+			'a4' => 0,
+			'b4' => 0,
+			'c4' => 0,
+			'd4' => 0,
+			'e4' => 0,
+			'f4' => 0,
+			'g4' => 0,
+			'h4' => 0,
+			
+			'a3' => 0,
+			'b3' => 0,
+			'c3' => 0,
+			'd3' => 0,
+			'e3' => 0,
+			'f3' => 0,
+			'g3' => 0,
+			'h3' => 0,
+
+			'a2' => 9817,
+			'b2' => 9817,
+			'c2' => 9817,
+			'd2' => 9817,
+			'e2' => 9817,
+			'f2' => 9817,
+			'g2' => 9817,
+			'h2' => 9817,
+
+			'a1' => 9814,
+			'b1' => 9816,
+			'c1' => 9815,
+			'd1' => 9813,
+			'e1' => 9812,
+			'f1' => 9815,
+			'g1' => 9816,
+			'h1' => 9814,
+		);
+		
+		
+		//array för att konvertera bokstav till siffra för beräkning och logiska vilkor
+		$yPosToInt = array(
+			'a' => 1,
+			'b' => 2,
+			'c' => 3,
+			'd' => 4,
+			'e' => 5,
+			'f' => 6,
+			'g' => 7,
+			'h' => 8,
+		);
+		
 		//funktion som returnar inskickat tal som posetivt oavset om det var negativt eller posetivt
 		function negToPos($number){
 			if ($number < 0) {
@@ -13,7 +100,7 @@
 		}
 		
 		//funktion som retrunar en array med antal steg i både x och y led
-		function xySteps(){
+		function xySteps($from, $to, $yPosToInt){
 			$xystep = array(
 			'y' => negToPos($yPosToInt[substr($to, 0, 1)] - $yPosToInt[substr($from, 0, 1)]),//räknar antal y leds steg
 			'x' => negToPos(substr($to, 1, 1) - substr($from, 1, 1))//räknar antal x leds steg
@@ -21,7 +108,7 @@
 			return $xystep;
 		}
 		
-		function checkCollision($moveOverArray){
+		function checkCollision($moveOverArray, $board){
 			foreach ($moveOverArray as $node) {
 				if($board[$node] != 0){
 					return false;
@@ -30,7 +117,7 @@
 			return true;
 		}
 		
-		function makeMoveOverArray($from, $to){
+		function makeMoveOverArray($from, $to, $yPosToInt){
 				
 			if(($yPosToInt[substr($from, 0, 1)] - $yPosToInt[substr($to, 0, 1)]) < 0){
 				$y = 1;
@@ -40,23 +127,25 @@
 			}
 			
 			if((substr($to, 1, 1) - substr($from, 1, 1)) < 0){
-				$x = 1;
+				$x = -1;
 			}
 			else{
-				$x = -1;
+				$x = 1;
 			}
 			
 			$moveOverAttay = array();
 			$from = array_search(($yPosToInt[substr($from, 0, 1)] + $y), $yPosToInt) . (substr($from, 1, 1) + $x);
-			
+			echo $from. "</br>";
 			while($from != $to){
 				$moveOverAttay[] = $from;
-				$from = "" . array_search(($yPosToInt[substr($from, 0, 1)] + $y), $yPosToInt) . (substr($from, 1, 1) + $x);
+				$tempFrom = array_search(($yPosToInt[substr($from, 0, 1)] + $y), $yPosToInt) . (substr($from, 1, 1) + $x);
+				$from = $tempFrom;
+				
 			}
 			return $moveOverAttay;
 		}
 		
-		function checkTo($piece, $to){
+		function checkTo($piece, $to, $board){
 			if($board[$to] != 0){
 				//kollar om pjäs är vit
 				if($piece >= 9812 && $piece <= 9817 ){
@@ -74,24 +163,14 @@
 			return true;
 		}
 		
-		//array för att konvertera bokstav till siffra för beräkning och logiska vilkor
-		$yPosToInt = array(
-			'a' => 1,
-			'b' => 2,
-			'c' => 3,
-			'd' => 4,
-			'e' => 5,
-			'f' => 6,
-			'g' => 7,
-			'h' => 8,
-		);
+		
 		
     	//black pawn
 		if($piece == 9823){
 			//här ska går diagonalt för att döda
 			if(substr($to, 0, 1) == array_search(($yPosToInt[substr($from, 0, 1)] + 1)) || substr($to, 0, 1) == array_search(($yPosToInt[substr($from, 0, 1)] - 1))){//här ska går diagonalt för att döda
 				if(substr($to, 1, 1) == (substr($from, 1, 1)) - 1){
-					if(checkTo($piece, $to)){
+					if(checkTo($piece, $to, $board)){
 						return true;
 					}
 				}
@@ -99,7 +178,7 @@
 			//drag för bondens ifrån startposion
 			else if(substr($from, 1,1) == 7 && array_search(substr($from, 0,1)) == array_search(substr($to, 0,1))){
 				if(substr($to, 1,1) == 6 || substr($to, 1,1) == 5){
-					if(checkCollision(makeMoveOverArray($from, $to))){
+					if(checkCollision(makeMoveOverArray($from, $to, $yPosToInt), $board)){
 						if($board[$to] == 0){
 							return true;
 						}
@@ -123,7 +202,7 @@
 			//här ska går diagonalt för att döda
 			if(substr($to, 0, 1) == array_search(($yPosToInt[substr($from, 0, 1)] + 1)) || substr($to, 0, 1) == array_search(($yPosToInt[substr($from, 0, 1)] - 1))){//här ska går diagonalt för att döda
 				if(substr($to, 1, 1) == (substr($from, 1, 1)) + 1){
-					if(checkTo($piece, $to)){
+					if(checkTo($piece, $to, $board)){
 						return true;
 					}
 				}
@@ -131,7 +210,7 @@
 			//drag för bondens ifrån startposion
 			else if(substr($from, 1,1) == 2 && array_search(substr($from, 0,1)) == array_search(substr($to, 0,1))){
 				if(substr($to, 1,1) == 3 || substr($to, 1,1) == 4){
-					if(checkCollision(makeMoveOverArray($from, $to))){
+					if(checkCollision(makeMoveOverArray($from, $to, $yPosToInt), $board)){
 						if($board[$to] == 0){
 							return true;
 						}
@@ -151,11 +230,11 @@
 		}
 		//rook
 		if($piece == 9820 || $piece == 9814){
-			$xyarray = xySteps();
+			$xyarray = xySteps($from, $to ,$yPosToInt);
 			//om en rikning är noll får andra vara hur lång som helst
 			if($xyarray["y"] == 0 || $xyarray["x"] == 0){
-				if(checkCollision(makeMoveOverArray($from, $to))){
-					if(checkTo($piece, $to)){
+				if(checkCollision(makeMoveOverArray($from, $to), $board)){
+					if(checkTo($piece, $to, $board)){
 						return true;
 					}
 				}
@@ -166,12 +245,12 @@
 		}
 		//bishop
 		if($piece == 9821 || $piece == 9815){
-			$xyarray = xySteps();
+			$xyarray = xySteps($from, $to ,$yPosToInt);
 			
 			//om det är lika många steg i båda riktningarna är det en diagonal förflyttning
 			if($xyarray["y"] == $xyarray["x"]){
-				if(checkCollision(makeMoveOverArray($from, $to))){
-					if(checkTo($piece, $to)){
+				if(checkCollision(makeMoveOverArray($from, $to, $yPosToInt), $board)){
+					if(checkTo($piece, $to, $board)){
 						return true;
 					}
 				}
@@ -182,9 +261,9 @@
 		}
 		//knight
 		if($piece == 9822 || $piece == 9816){
-			$xyarray = xySteps();
+			$xyarray = xySteps($from, $to ,$yPosToInt);
 			if(($xyarray["x"] == 1 && $xyarray["y"] == 2) || ($xyarray["x"] == 2 && $xyarray["y"] == 1)){
-				if(checkTo($piece, $to)){
+				if(checkTo($piece, $to, $board)){
 					return true;
 				}
 			}
@@ -194,10 +273,10 @@
 		}
 		//queen
 		if($piece == 9819 || $piece == 9813){
-			$xyarray = xySteps();
+			$xyarray = xySteps($from, $to ,$yPosToInt);
 			if($xyarray["x"] == $xyarray["y"] || ($xyarray["x"] == 0 xor $xyarray["y"] == 0)){
 				if(checkCollision(makeMoveOverArray($from, $to))){
-					if(checkTo($piece, $to)){
+					if(checkTo($piece, $to, $board)){
 						return true;
 					}
 				}
@@ -209,11 +288,11 @@
 		}
 		//king
 		if($piece == 9818 || $piece == 9812){
-			$xyarray = xySteps();
+			$xyarray = xySteps($from, $to ,$yPosToInt);
 			//om det är lika många steg i båda riktningarna är det en diagonal förflyttning
 			if($xyarray["y"] <= 1 && $xyarray["x"] <= 1){
-				if(checkCollision(makeMoveOverArray($from, $to))){
-					if(checkTo($piece, $to)){
+				if(checkCollision(makeMoveOverArray($from, $to, $yPosToInt), $board)){
+					if(checkTo($piece, $to, $board)){
 						return true;
 					}
 				}
@@ -224,4 +303,40 @@
 		}
     }
     
+	
+	
+	//*********************************************************************************************************//
+	
+			
+		/*
+		if(checkMove('9816','b1','a3')){
+			echo("funkarhar hurra</br>");
+		}
+		else{
+			echo "buuuuuuuuuuuu</br>";
+		}
+	
+		if(checkMove('9821','c8','a6')){
+			echo("funkarhar hurra</br>");
+		}
+		else{
+			echo "buuuuuuuuuuuu</br>";
+		}
+	if(checkMove('9817','b2','b3')){
+			echo("funkarhar hurra</br>");
+		}
+		else{
+			echo "buuuuuuuuuuuu</br>";
+		}
+	if(checkMove('9817','c2','c4')){
+			echo("funkarhar hurra</br>");
+		}
+		else{
+			echo "buuuuuuuuuuuu</br>";
+		}
+	
+	*/
+	
+	
+	
 ?>
